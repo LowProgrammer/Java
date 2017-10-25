7e72884cd6b78b565c6874fa9362c9ef6c301291
Administrator <1320954152@qq.com>
2017-10-26 00:10:08 +0800

    归并排序递归的一些问题

--- src/com/henu/feifei/algorithm/Sort.java
+++ src/com/henu/feifei/algorithm/Sort.java
@@ -10,9 +10,11 @@ public class Sort {
 		//int[] arr= {36,25,26,12,23,24,28,50,64,57,41,68,91,5};
 		//int[] arr= {10,9,8,7,6,5,4,3,2,1,0};
 		int[] arr= {6,7,8,9,10,0,1,2,3,4,5};
+		int[] arr2= {5,2,4,7,1,3,2,6};
 		Sort sortInsert=new Sort();
 		//sortInsert.insertSort(arr);
-		sortInsert.mergeInsert(arr);
+		//sortInsert.mergeInsert(arr);
+		sortInsert.merage(arr2, 0, arr2.length);
 	}
 	//插入排序
 	public void insertSort(int[] arr) {
@@ -31,21 +33,21 @@ public class Sort {
 		}
 	}
 	
-	public void mergeInsert(int[] arr) {
+	public void mergeInsert(int[] arr,int p,int q,int r) {
 		int[] ss=arr;
-		int len=arr.length;
-		int medium=len/2;
-		int[] larr=new int[medium];
-		int[] rarr=new int[len-medium];
-		for(int i=0;i<medium;i++) {
-			larr[i]=arr[i];
+		int n1=q-p+1;
+		int n2=r-q;
+		int[] larr=new int[n1];
+		int[] rarr=new int[n2];
+		for(int i=0;i<n1;i++) {
+			larr[i]=arr[p+i];
 		}
-		for(int i=medium;i<len;i++) {
-			rarr[i-medium]=arr[i];
+		for(int i=0;i<n2;i++) {
+			rarr[i]=arr[q+i+1];
 		}
 		int a=0;
 		int b=0;
-		for(int i=0;i<len;i++) {
+		for(int i=p;i<r+1;i++) {
 			if(a<larr.length&&b<rarr.length) {
 				if(larr[a]<=rarr[b]) {
 					ss[i]=larr[a];
@@ -66,6 +68,15 @@ public class Sort {
 		}
 		for(int s:ss) {
 			System.out.println(s);
+		}
+	}
+	public void merage(int[] arr,int p,int r) {
+		if(p<r) {
+			int q=(p+r)/2;
+			System.out.println(p+"<=====>"+q+"<=======>"+r);
+			merage(arr, p, q);
+			merage(arr, q+1, r);
+			mergeInsert(arr, p, q, r);
 		}
 	}
 }
