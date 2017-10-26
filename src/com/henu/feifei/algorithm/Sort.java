7505fbe704e03f77b79de37851122f03bbb84ba5
1320954152@qq.com <chen19960927>
2017-10-26 19:44:57 +0800

    插入排序和归并排序的递归实现

--- src/com/henu/feifei/algorithm/Sort.java
+++ src/com/henu/feifei/algorithm/Sort.java
@@ -1,4 +1,11 @@
 package com.henu.feifei.algorithm;
+
+import static com.henu.feifei.algorithm.Sort.print;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * descri 插入排序
  * @author Administrator
@@ -6,15 +13,12 @@ package com.henu.feifei.algorithm;
  */
 public class Sort {
 	public static void main(String[] args) {
-//		int[] arr= {12,25,26,12,23,24,28,50,64,57,41,68,91};
-		//int[] arr= {36,25,26,12,23,24,28,50,64,57,41,68,91,5};
-		//int[] arr= {10,9,8,7,6,5,4,3,2,1,0};
-		int[] arr= {6,7,8,9,10,0,1,2,3,4,5};
-		int[] arr2= {5,2,4,7,1,3,2,6};
 		Sort sortInsert=new Sort();
-		//sortInsert.insertSort(arr);
-		//sortInsert.mergeInsert(arr);
-		sortInsert.merage(arr2, 0, arr2.length);
+		int[] A= {7,6,5,4,3,2,1,0};
+		sortInsert.insertionSert(A, 7);
+		print(A);
+		
+		
 	}
 	//插入排序
 	public void insertSort(int[] arr) {
@@ -32,51 +36,92 @@ public class Sort {
 			System.out.println(x);
 		}
 	}
-	
-	public void mergeInsert(int[] arr,int p,int q,int r) {
-		int[] ss=arr;
-		int n1=q-p+1;
-		int n2=r-q;
-		int[] larr=new int[n1];
-		int[] rarr=new int[n2];
-		for(int i=0;i<n1;i++) {
-			larr[i]=arr[p+i];
+	//插入排序递归实现
+	public static void insert(int[] arr,int n) {
+		int i=n-1;
+		int key=arr[n];
+		while(i>=0&&arr[i]>key) {
+			arr[i+1]=arr[i];
+			i--;
 		}
-		for(int i=0;i<n2;i++) {
-			rarr[i]=arr[q+i+1];
-		}
-		int a=0;
-		int b=0;
-		for(int i=p;i<r+1;i++) {
-			if(a<larr.length&&b<rarr.length) {
-				if(larr[a]<=rarr[b]) {
-					ss[i]=larr[a];
-					a++;
-				}else{
-					ss[i]=rarr[b];
-					b++;
-				}
-			}else {
-				if(!(a<larr.length)) {
-					ss[i]=rarr[b];
-					b++;
-				}else {
-					ss[i]=larr[a];
-					a++;
-				}
-			}
-		}
-		for(int s:ss) {
-			System.out.println(s);
+		arr[i+1]=key;
+	} 
+	public static void insertionSert(int[] arr,int index) {
+		if(index>0) {
+			insertionSert(arr, index-1);
+			insert(arr, index);
 		}
 	}
-	public void merage(int[] arr,int p,int r) {
+	//归并排序--递归
+	public static void mergeInsert(int[] arr,int p,int q,int r) {
+		int len=r-p+1;//0-------1-----------3
+		int a=q-p+1;
+		int b=len-a;
+		int[] arrL=new int[a];
+		int[] arrR=new int[b];
+		for(int i=0;i<a;i++) {
+			arrL[i]=arr[p+i];
+		}
+		for(int i=0;i<b;i++) {
+			arrR[i]=arr[q+1+i];
+		}
+		int m=0,n=0;
+		for(int i=p;i<r+1;i++) {
+			if (m<a&&n<b) {
+				if(arrL[m]<arrR[n]) {
+					arr[i]=arrL[m];
+					m++;
+				}else {
+					arr[i]=arrR[n];
+					n++;
+				}
+			}
+			else {
+				if(m==a&&n<b) {
+					arr[i]=arrR[n];
+					n++;
+				}
+				if(n==b&&m<a) {
+					arr[i]=arrL[m];
+					m++;
+				}
+			}
+			
+		}
+		print(arr);
+	}
+	public static void merage(int[] arr,int p,int r) {
 		if(p<r) {
+			
 			int q=(p+r)/2;
 			System.out.println(p+"<=====>"+q+"<=======>"+r);
 			merage(arr, p, q);
 			merage(arr, q+1, r);
 			mergeInsert(arr, p, q, r);
 		}
+		//print(arr);
+	}
+	
+	
+	
+	//一些方法
+	public static void print(int[] arr) {
+		for(int a:arr) {
+			System.out.print(a+"   ");
+		}
+		System.out.println();
+	}
+	public static void print(String mess) {
+		System.out.println(mess);
+	}
+	public static void print(int a) {
+		System.out.println(a);
+	}
+	public static int[] subArr(int[] arr,int begin,int end) {
+		int[] temp=new int[end-begin];
+		for(int i=begin;i<end;i++) {
+			temp[i]=arr[i];
+		}
+		return temp;
 	}
 }
