b1aafdec173596ef118dcf56b05a8335341c3585
Administrator <1320954152@qq.com>
2017-10-29 23:42:53 +0800

    求两数之和在1s和16mib内存花销内

---
+++ src/com/henu/feifei/algorithm/Vijos.java
@@ -0,0 +1,17 @@
+package com.henu.feifei.algorithm;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Vijos {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		BigInteger a=new BigInteger(scanner.next());
+		BigInteger b=new BigInteger(scanner.next());
+		System.out.println(a.add(b));
+//		Scanner scanner=new Scanner(System.in);
+//		Short a=new Short(scanner.next());
+//		Short b=new Short(scanner.next());
+//		System.out.println((int)(a+b));
+	}
+}
