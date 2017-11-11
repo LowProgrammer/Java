c537f5806db3cd965186fd535ef619d83de60a58
1320954152@qq.com <chen19960927>
2017-11-11 21:13:58 +0800

    使用内部类隐藏线程

---
+++ src/com/henu/feifei/concurrency/ThreadVariations.java
@@ -0,0 +1,15 @@
+package com.henu.feifei.concurrency;
+/**
+	*@ClassName:ThreadVariations
+	*@Description:TODO
+	*@author:feifei
+	*@date :2017年11月11日-下午8:24:11
+	*@version:1.0
+	*/
+public class ThreadVariations {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
