f8e46ffee2a3eafe554a76e93ed59369846363b2
Administrator <1320954152@qq.com>
2017-11-12 12:49:36 +0800

    线程join方法的使用

---
+++ src/com/henu/feifei/concurrency/Joining.java
@@ -0,0 +1,55 @@
+package com.henu.feifei.concurrency;
+
+import com.henu.feifei.utils.Print;
+
+class Sleeper extends Thread{
+	private int duration;
+	public Sleeper(String name,int sleeptime) {
+		super(name);
+		this.duration=sleeptime;
+		start();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			sleep(duration);
+		} catch (Exception e) {
+			Print.print(getName()+" was interrupted ,"+"isInterrupted():"+isInterrupted());
+			// TODO: handle exception
+		}
+		Print.print(getName()+" has awakened");
+	}
+}
+class Joiner extends Thread{
+	private Sleeper sleeper;
+	public Joiner(String name,Sleeper sleeper) {
+		super(name);
+		this.sleeper=sleeper;
+		start();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			sleeper.join();
+		} catch (Exception e) {
+			Print.print("interrupted");
+			// TODO: handle exception
+		}
+		Print.print(getName()+" join completed");
+	}
+}
+public class Joining {
+	public static void main(String[] args) {
+		Sleeper
+			sleepy=new Sleeper("sleepy", 1500),
+			grumpy=new Sleeper("grumpy", 1500);
+		Joiner
+			dopey=new Joiner("Dopey", sleepy),
+			doc=new Joiner("Doc",grumpy);
+		grumpy.interrupt();
+	}
+}
